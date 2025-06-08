ccf58f18d02714df0d02920129322677f97b5e2f
rezearcher <rez.archer@gmail.com>
2025-06-08 13:54:30 -0500

    Style: Apply black and isort to moviepy_compat.py

--- app/services/moviepy_compat.py
+++ app/services/moviepy_compat.py
@@ -10,6 +10,7 @@ logger = logging.getLogger(__name__)
 # Set MoviePy as unavailable
 MOVIEPY_AVAILABLE = False
 
+
 # Define dummy classes to prevent import errors elsewhere
 class DummyClip:
     def __init__(self, *args, **kwargs):
@@ -23,6 +24,7 @@ class DummyClip:
 
     def write_videofile(self, *args, **kwargs):
         pass
+
 
 # Define all the dummy imports needed by other modules
 AudioFileClip = ImageClip = VideoFileClip = CompositeVideoClip = TextClip = DummyClip
